abaa70b01d23eca561d6bb40ddf609f164ce1bb7
Amie <0950080034@sv.hcmunre.edu.vn>
2023-05-26 14:03:50 +0700

    update

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+
 let getHomePage = (req, res) => {
   return res.send("xin chào mn");
 };
 let getWebhook = (req, res) => {
-  let VERIFY_TOKEN = process.env.VERIFY_TOKEN;
   // Parse the query params
   let mode = req.query["hub.mode"];
   let token = req.query["hub.verify_token"];
@@ -25,11 +27,38 @@ let getWebhook = (req, res) => {
 };
 
 let postWebhook = (req, res) => {
+  // Parse the request body from the POST
   let body = req.body;
 
-  console.log(`\u{1F7EA} Received webhook:`);
-  console.dir(body, { depth: null });
+  // Check the webhook event is from a Page subscription
+  if (body.object === "page") {
+    // Iterate over each entry - there may be multiple if batched
+    body.entry.forEach(function (entry) {
+      // Gets the body of the webhook event
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log("Sender PSID: " + sender_psid);
+    });
+
+    // Return a '200 OK' response to all events
+    res.status(200).send("EVENT_RECEIVED");
+  } else {
+    // Return a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
 };
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {}
 
 module.exports = {
   getHomePage: getHomePage,
