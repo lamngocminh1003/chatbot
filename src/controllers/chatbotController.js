31e5d7d316fa2a3b30e852eb18e0fa76e9d7fc07
Amie <0950080034@sv.hcmunre.edu.vn>
2023-05-26 23:02:57 +0700

    update

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -120,6 +120,8 @@ async function handlePostback(sender_psid, received_postback) {
     case "no":
       response = { text: "Oops, try sending another image." };
       break;
+      
+    case "RESTART_BOT":
     case "GET_STARTED":
       await chatbotService.handleGetStarted(sender_psid);
       break;
