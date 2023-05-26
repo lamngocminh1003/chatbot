3d45d546b5bc9c78455578379341dfc80127c648
Amie <0950080034@sv.hcmunre.edu.vn>
2023-05-26 12:42:40 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,22 @@
+import express  from "express";
+import viewEngine from "./config/viewEngine";
+import bodyParser from "body-parser";
+import initWebRoute from "./routes/web"
+require("dotenv").config();
+
+let app = express();
+
+//config view engine
+viewEngine(app);
+
+//parse request to json
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+//init web routes
+initWebRoute(app);
+
+let port = process.env.PORT || 8080;
+app.listen(port,()=>{
+    console.log('chatbot is running: ', port);
+})
